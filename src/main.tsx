5a35c47e0389d79598cd363f7eb1d141621c04a6
Ethan Lemke <ethan@mfbtech.com>
2025-02-12 17:44:44 +0000

    added logic to preload pspdfkit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import PSPDFKit from "pspdfkit";
 
+export async function preloadPsPdfKit(): Promise<void> {
+  return PSPDFKit.preloadWorker({
+    container: "",
+    document: "",
+    baseUrl: `${window.location.protocol}//${window.location.host}/`,
+    inlineWorkers: false,
+  });
+}
+
+preloadPsPdfKit().catch(console.error);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
