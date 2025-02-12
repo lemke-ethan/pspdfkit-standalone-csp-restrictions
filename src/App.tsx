aaa5f027d3a0d82806017dd6c6a50438eab76169
Ethan Lemke <ethan@mfbtech.com>
2025-02-12 16:36:55 +0000

    init pdf viewer, example pdf, and pdf asset script

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import "./App.css";
+import { PDFViewer } from "./PdfViewer";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Example App</h1>
+      <PDFViewer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/PdfViewer.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+import PSPDFKit, { Instance } from "pspdfkit";
+
+export function PDFViewer() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const pdfInstanceRef = useRef<Instance | "init" | "pending">("init");
+
+  useEffect(() => {
+    (async () => {
+      if (containerRef.current && pdfInstanceRef.current === "init") {
+        pdfInstanceRef.current = "pending";
+        pdfInstanceRef.current = await PSPDFKit.load({
+          container: containerRef.current,
+          document: "why-functional-programming.pdf",
+          baseUrl: `${window.location.protocol}//${window.location.host}/`, // Set your base URL
+        });
+      }
+    })();
+
+    return () => {
+      if (
+        pdfInstanceRef.current !== "init" &&
+        pdfInstanceRef.current !== "pending"
+      ) {
+        PSPDFKit.unload(containerRef.current!);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      className="pdf-viewer"
+      ref={containerRef}
+      style={{ height: "100vh" }}
+    />
+  );
+}
